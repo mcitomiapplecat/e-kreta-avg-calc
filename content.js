07d5a372a02e138c59175677e78d30b3166f7e80
mcitomi <mciapple22@gmail.com>
2023-05-17 23:27:39 +0200

    public

---
+++ content.js
@@ -0,0 +1,29 @@
+console.log("[mcitomi] Kréta átlag számító betöltve. v.23.04.04");
+
+const display = document.getElementsByClassName("k-footer-template");
+let avg = 0;
+
+if(document.URL.includes("TanuloErtekeles/Osztalyzatok")){
+    
+    console.log("[mcitomi] Átlag számolása...");
+
+    let elements = document.getElementsByClassName("atlag kozepre");
+    const checktable = setInterval(() => {
+        if(elements.length != 0) {
+            clearInterval(checktable);
+            write(elements);
+        }
+    }, 50);
+}
+
+function write(elements) {
+    let sum = 0, db = 0;
+    for (let i = 0; i < elements.length; i++) {
+        if(elements.item(i).textContent != "") {
+            sum += Number((elements.item(i).textContent).replace(",","."));
+            db++;
+        } 
+    }
+    display.item(0).children[display.item(0).childElementCount - 2].textContent = `${(sum / db).toFixed(2)}`;
+    console.log("[mcitomi] Átlag kiszámolva.");
+}
